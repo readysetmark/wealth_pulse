8f8318e31c10f292ae8f8400fda54d519780ab34
Mark Williams <mark@readysetmark.com>
2017-06-10 12:08:31 -0500

    Use environment vars
    Use environment variables to determine location of price and ledger files.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ extern crate wealth_pulse;
 
 //use wealth_pulse::parser::chomp::{parse_pricedb};
 use wealth_pulse::parser::combine::{parse_pricedb, parse_ledger};
+use std::env;
 
 // MAIN
 
 fn main() {
-    let pricedb_filepath = "/Users/mark/Nexus/Documents/finances/ledger/.pricedb";
-    let ledger_filepath = "/Users/mark/Nexus/Documents/finances/ledger/ledger.dat";
+    let pricedb_filepath = env::var("WEALTH_PULSE_PRICES_FILE")
+        .expect("Could not read WEALTH_PULSE_PRICES_FILE environment variable");
+    let ledger_filepath = env::var("LEDGER_FILE")
+        .expect("Could not read LEDGER_FILE environment variable");
 
-    let prices = parse_pricedb(pricedb_filepath);
+    let prices = parse_pricedb(&pricedb_filepath);
     println!("Parsed {} prices", prices.len());
 
-    let tree = parse_ledger(ledger_filepath);
+    let tree = parse_ledger(&ledger_filepath);
     println!("Parsed {} tree items", tree.len());
 
     // for price in &prices {
